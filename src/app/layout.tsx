8de60c311f1cc4faf153d24444de246d8d4d5d7b
subodhwork21 <subodhac.work@gmail.com>
2024-05-02 15:40:13 +0545

    new commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next";
 // import { Inter } from "next/font/google";
 import "./globals.css";
 import localFont from "next/font/local";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+import Header from "@/app/components/Header";
+import Footer from "@/app/components/Footer";
 
 export const metadata: Metadata = {
   title: "Create Next App",
