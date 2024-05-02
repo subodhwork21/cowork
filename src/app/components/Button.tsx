25fc55e2e92644410b0c7448805e68ad71792531
subodhwork21 <subodhac.work@gmail.com>
2024-05-02 15:54:49 +0545

    try again

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -1,3 +1,5 @@
+"use client";
+import React from "react";
 interface btn {
   content: string;
   bgcolor: string;
@@ -17,3 +19,23 @@ const Button = ({ content, bgcolor }: btn) => {
 };
 
 export default Button;
+
+// interface btn {
+//   content: string;
+//   bgcolor: string;
+// }
+// const Button = ({ content, bgcolor }: btn) => {
+//   return (
+//     <>
+//       <button
+//         className={`${bgcolor} rounded-[42px] px-4 py-2 text-sm font-medium ${
+//           bgcolor === "bg-black" ? "text-white" : "text-black outline outline-1"
+//         }`}
+//       >
+//         {content}
+//       </button>
+//     </>
+//   );
+// };
+
+// export default Button;
