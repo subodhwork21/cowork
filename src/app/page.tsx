b6be2e252e8019492a70591dba2c9157da9641b5
subodhwork21 <subodhac.work@gmail.com>
2024-05-14 23:32:43 +0545

    fresh install

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,236 +1,113 @@
-import React from "react";
-import HeroPage from "@/app/components/hero";
-import BrandPage from "@/app/components/brand";
 import Image from "next/image";
-import Button from "@/app/components/button";
-const page = () => {
+
+export default function Home() {
   return (
-    <>
-      <HeroPage />
-      <BrandPage />
-      <div className="py-28 px-16 flex flex-col items-center justify-center gap-6">
-        <p className="text-[48px] font-bold">Why Choose Cowork?</p>
-        <div className="flex flex-row items-center justify-between w-full">
-          <div className="w-full flex flex-col justify-center items-center gap-4">
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+          Get started by editing&nbsp;
+          <code className="font-mono font-bold">src/app/page.tsx</code>
+        </p>
+        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
+          <a
+            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
+            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            By{" "}
             <Image
-              src={"/logos/planet.png"}
-              alt="planet"
-              width={48}
-              height={48}
-            ></Image>
-            <p className="text-center text-[24px]">Flexible Spaces</p>
-            <p className="text-center">
-              Whether you're a solopreneur, startup, or an established
-              enterprise, our flexible office solutions cater to your evolving
-              needs.
-            </p>
-          </div>
-          <div className="w-full flex flex-col justify-center items-center gap-4">
-            <Image
-              src={"/logos/CreditCard.png"}
-              alt="credit_card"
-              width={48}
-              height={48}
-            ></Image>
-            <p className="text-center text-[24px]">Transparent Pricing</p>
-            <p className="text-center">
-              Choose a plan that suits your budget and business objectives, and
-              experience the value of a premium coworking space without breaking
-              the bank.
-            </p>
-          </div>
-          <div className="w-full flex flex-col justify-center items-center gap-4">
-            <Image
-              src={"/logos/UserList.png"}
-              alt="user_list"
-              width={48}
-              height={48}
-            ></Image>
-            <p className="text-center text-[24px]">Tailored Memberships</p>
-            <p className="text-center">
-              Whether you prefer the flexibility of a hot desk or the
-              exclusivity of a private office, Cowork offers tailored solutions
-              to suit every working style.
-            </p>
-          </div>
+              src="/vercel.svg"
+              alt="Vercel Logo"
+              className="dark:invert"
+              width={100}
+              height={24}
+              priority
+            />
+          </a>
         </div>
       </div>
-      <section className="w-full flex flex-col items-center justify-center">
-        <div className="max-w-[768px] w-full flex flex-col items-center justify-center gap-4">
-          <span>-</span>
-          <p className="text-[12px] font-bold">A Virtual Tour</p>
-          <p className="text-[48px]">Explore Cowork Through Our Lens</p>
-          <p className="text-center text-[13px]">
-            Experience the essence of Cowork before setting foot in our dynamic
-            spaces. Our immersive video tour gives you a sneak peek into the
-            vibrant atmosphere, cutting-edge facilities, and collaborative
-            energy that define the Cowork experience.
+
+      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
+        <Image
+          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+          src="/next.svg"
+          alt="Next.js Logo"
+          width={180}
+          height={37}
+          priority
+        />
+      </div>
+
+      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
+        <a
+          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className="mb-3 text-2xl font-semibold">
+            Docs{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className="m-0 max-w-[30ch] text-sm opacity-50">
+            Find in-depth information about Next.js features and API.
           </p>
-          <Button content="Explore Spaces" bgcolor="bg-white" />
-        </div>
+        </a>
 
-        <div className="mt-20 relative">
-          <Image
-            src={"/images/Image.png"}
-            width={1312}
-            height={700}
-            alt="videoimage"
-          ></Image>
-          <div className="absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%]">
-            <Image
-              src={"/logos/Play Button.png"}
-              alt="play_btn"
-              height={128}
-              width={102.4}
-            ></Image>
-          </div>
-        </div>
-
-        <div className="mt-20 flex flex-row items-center justify-center px-16 gap-4 py-28">
-          <div>
-            <p className="text-[12px] uppercase">Cowork in Numbers</p>
-            <p className="text-[48px] font-bold">
-              Transformative Statistics That Speak Volumes
-            </p>
-            <div className="grid grid-cols-2 gap-4 mt-10">
-              <div>
-                <p className="text-[48px] font-bold">240%</p>
-                <p>Community Growth</p>
-              </div>
-              <div>
-                <p className="text-[48px] font-bold">99%</p>
-                <p> Technology Uptime </p>
-              </div>
-              <div>
-                <p className="text-[48px] font-bold">50%</p>
-                <p>Happy Members</p>
-              </div>
-              <div>
-                <p className="text-[48px] font-bold">100%</p>
-                <p>Renewable Energy Sources</p>
-              </div>
-            </div>
-          </div>
-          <Image
-            src={"/images/statImage.png"}
-            alt="statImage"
-            width={616}
-            height={640}
-          ></Image>
-        </div>
-
-        <div className="px-16 py-28">
-          <p className="text-[12px] w-100 text-center">Cowork in Words</p>
-          <p className="text-[48px] w-100 text-center">
-            Hear It from Our Clients
+        <a
+          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className="mb-3 text-2xl font-semibold">
+            Learn{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className="m-0 max-w-[30ch] text-sm opacity-50">
+            Learn about Next.js in an interactive course with&nbsp;quizzes!
           </p>
-          <div className="flex flex-row justify-between items-center gap-16 mt-10 px-[88px] ">
-            <div className="py-8 px-8 gap-6 flex flex-col justify-center items-center bg-lightGreen rounded-[24px] flex-1">
-              <Image
-                className="rounded-full aspect-square object-cover object-right-top"
-                src={"/images/person1.avif"}
-                width={56}
-                height={56}
-                alt="person1"
-              ></Image>
-              <p className="text-left">
-                "The 24/7 access and secure facilities have been incredibly
-                convenient for my team's flexible schedules. We love the
-                coworking space!"
-              </p>
-              <div className="w-full">
-                <p className="text-center font-bold">Liam Brown</p>
-                <p className="text-center">
-                  Software Engineer, TechStartup Innovations
-                </p>
-              </div>
-            </div>
-            <div className="py-8 px-8 gap-6 flex flex-col justify-center items-center bg-lightGray rounded-[24px] mb-16  flex-1">
-              <Image
-                className="rounded-full aspect-square object-cover object-right-top"
-                src={"/images/person2.png"}
-                width={56}
-                height={56}
-                alt="person1"
-              ></Image>
-              <p className="text-left">
-                “The aesthetics of Cowork are inspiring. The attention to detail
-                in the design creates an atmosphere that sparks creativity. It's
-                a place where ideas flow effortlessly, and collaboration happens
-                organically.”
-              </p>
-              <div className="w-full">
-                <p className="text-center font-bold">Liam Brown</p>
-                <p className="text-center">
-                  Software Engineer, TechStartup Innovations
-                </p>
-              </div>
-            </div>
-            <div className="py-8 px-8 gap-6 flex flex-col justify-center items-center bg-skyBlue rounded-[24px] mt-4 flex-1">
-              <Image
-                className="rounded-full aspect-square object-cover object-right-top"
-                src={"/images/person3.avif"}
-                width={56}
-                height={56}
-                alt="person1"
-              ></Image>
-              <p className="text-left">
-                "As a freelance designer, I was getting tired of working from
-                home or coffee shops. The coworking space has provided me with a
-                productive and professional environment to focus on my work."
-              </p>
-              <div className="w-full">
-                <p className="text-center font-bold">Michael Thompson</p>
-                <p className="text-center">Graphic Designer, DesignCo</p>
-              </div>
-            </div>
-          </div>
-          <div className="flex flex-row justify-center items-center gap-16 mt-10 px-[88px] ">
-            <div className="py-8 px-8 gap-6 flex flex-col justify-center items-center bg-shinyPink rounded-[24px] w-[360px]">
-              <Image
-                className="rounded-full aspect-square object-cover object-right-top"
-                src={"/images/person1.avif"}
-                width={56}
-                height={56}
-                alt="person1"
-              ></Image>
-              <p className="text-left">
-                "The coworking space has been a wonderful resource for my team.
-                The open floor plan and dedicated private offices allow us to
-                collaborate and concentrate as needed."
-              </p>
-              <div className="w-full">
-                <p className="text-center font-bold">David Wilson</p>
-                <p className="text-center">
-                  Project Manager, SoftwareSolutions LLC
-                </p>
-              </div>
-            </div>
-            <div className="py-8 px-8 gap-6 flex flex-col justify-center items-center bg-lightOrange rounded-[24px] mt-16  w-[360px]">
-              <Image
-                className="rounded-full aspect-square object-cover object-right-top"
-                src={"/images/person2.png"}
-                width={56}
-                height={56}
-                alt="person1"
-              ></Image>
-              <p className="text-left">
-                "The flexible membership options and amenities like high-speed
-                internet, printers, and meeting rooms have made this coworking
-                space a perfect fit for my small business."
-              </p>
-              <div className="w-full">
-                <p className="text-center font-bold"> Alex Nguyen</p>
-                <p className="text-center">
-                  Marketing Consultant, Maverick Marketing
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-    </>
+        </a>
+
+        <a
+          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className="mb-3 text-2xl font-semibold">
+            Templates{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className="m-0 max-w-[30ch] text-sm opacity-50">
+            Explore starter templates for Next.js.
+          </p>
+        </a>
+
+        <a
+          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className="mb-3 text-2xl font-semibold">
+            Deploy{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
+            Instantly deploy your Next.js site to a shareable URL with Vercel.
+          </p>
+        </a>
+      </div>
+    </main>
   );
-};
-
-export default page;
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,26 +1,20 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  theme: {
-   
-   extend: {
-    colors: {
-      lightGreen: "#D3F985",
-      lightGray: "#F1F1F1",
-      skyBlue: "#A6E8F6",
-      shinyPink: "#FCBEDC",
-      lightOrange: "#FBC899"
-    },
-   },
-  
- },
+import type { Config } from "tailwindcss";
+
+const config: Config = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
- 
-    // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
- 
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
+  },
   plugins: [],
-}
+};
+export default config;
