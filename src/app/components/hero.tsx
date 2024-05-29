e10e9f9d7fb7d5f469ffcccd672c07f275dafb21
subodhwork21 <subodhac.work@gmail.com>
2024-05-29 21:30:58 +0545

    simple fix in hero page

--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -2,9 +2,9 @@ import Image from "next/image";
 import Button from "@/app/components/button";
 const HeroPage = () => {
   return (
-    <div className="flex lg:flex-row flex-col items-center justify-between md:py-28 py-4 px-16 ">
+    <div className="flex lg:flex-row flex-col items-center justify-between md:py-28 py-4 md:px-16 px-5">
       <div className="lg:w-[50%] w-full flex flex-col lg:items-start lg:justify-start items-center justify-center lg:mb-none mb-12">
-        <h1 className="lg:text-[96px]  lg:leading-[100px] font-bold mb-8 md:text-[50px] text-[50px] lg:text-left text-center leading-[60px]">
+        <h1 className="lg:text-[96px]  lg:leading-[100px] font-bold mb-8 md:text-[50px] text-[48px] lg:text-left text-center leading-[60px]">
           Elevate Your Workspace with Cowork
         </h1>
         <p className="lg:w-[480px] w-full lg:mb-8 mb-12 lg:leading-[40px]leading-[40px] text-center lg:text-left">
@@ -15,7 +15,7 @@ const HeroPage = () => {
         </p>
         <Button bgcolor="bg-black" content="Claim Your Spot" />
       </div>
-      <div className="lg:w-[50%] w-full">
+      <div className="lg  :w-[50%] w-full">
         <Image
           className="object-fill"
           src={"/images/heroimage.png"}
